f824f83b46eca77ac7262a6d592859787f64f1c1
Savo Lazic <lazicsavo@gmail.com>
2014-10-16 19:38:12 -0400

    Fixed up MgProxy.py. It was spaced using tabs (oops) and removed the debug comments.

--- MgProxy.py
+++ MgProxy.py
@@ -1,18 +1,13 @@
 import sys
 
 if sys.version_info[0] != 2:
-	sys.stderr.write('MgProxy only works with Python 2')
-	sys.exit()
+    sys.stderr.write('MgProxy only works with Python 2')
+    sys.exit()
 
 from src.mg_proxy_creator import main
 
 if __name__ == "__main__":
-	'''Run the main program. Uncaught errors are displayed to the user.
-	TODO: Also create a file with more detailed error report.
-	'''
-	# try:
-	# 	main()
-	# except:
-	# 	error_msg = 'Unexpected error of type ' + str(sys.exc_info()[0]) + '\n'
-	# 	sys.stderr.write(error_msg)
-	main()
+    '''Run the main program. Uncaught errors are displayed to the user.
+    TODO: Also create a file with more detailed error report.
+    '''
+    main()
