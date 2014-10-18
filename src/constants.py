37f909806f062c0077679ac5b80ead126374b716
Savo Lazic <lazicsavo@gmail.com>
2014-10-18 16:11:36 -0400

    JSON file can be successfully download and parsed.

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,8 @@ DPI = 300  # Default DPI
 WIDTH, HIGHT = 2.49, 3.48  # Default card width and hight in inches
 PAGE_X, PAGE_Y = 4, 2  # Default number of cards per jpg file
 BASE_URL = 'http://mtgimage.com/'  # URL of image database
+BASE_URL_JSON = 'http://mtgjson.com/'  # URL of JSON database
+JSON_EXT = '.json'  # Extension for json files on mtgjson.com
 
 
 class MgException(Exception):
--- src/get_mtg_json.py
+++ src/get_mtg_json.py
@@ -1,1 +1,37 @@
 '''Download and parse JSON files from mtgjson.com'''
+
+import urllib
+from urlparse import urljoin
+import json
+
+from .constants import MgException, BASE_URL_JSON, JSON_EXT
+
+
+def createSetAddress(set_name):
+    '''Crate a valid URL address to the JSON file for a given set'''
+    file_name = urllib.quote(set_name + JSON_EXT)
+    json_url = urljoin(BASE_URL_JSON, 'json/')
+    return urljoin(json_url, file_name)
+
+
+def getSetJson(set_name):
+    '''Downloads the JSON data for the given set and returns it as a dict'''
+    address = createSetAddress(set_name)
+    response = urllib.urlopen(address)
+
+    if response.getcode() != 200:
+        raise MgException(
+            'Set URL does not exist. Error code: ' + str(response.getcode()) +
+            ': ' + address)
+
+    if response.info()['Content-Type'] != 'application/json':
+        raise MgException(
+            'Expected json file, instead received: ' +
+            response.info()['Content-Type'] +
+            ': ' +
+            address)
+
+    json_size = int(response.info()['Content-Length'])
+    json_stream = response.read(json_size)
+
+    return json.loads(json_stream)
