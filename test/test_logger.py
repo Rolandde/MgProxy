898bf59cac044028c53d0e79fbc5ee3fd85a6891
Savo Lazic <lazicsavo@gmail.com>
2014-10-25 19:30:19 -0400

    Minor addition to logger testcase. At this point, my logging is way to complex and I'll go back to doing only module level logging. I'll also remove the .ropeproject folder from being tracket, as it can be rebuilt with ease.

--- test/test_logger.py
+++ test/test_logger.py
@@ -1,10 +1,11 @@
 '''Tests that the logging calls behave properly'''
 import unittest
+from .src.logger_dict import MG_LOGGER, MG_LOGGER_CONST, getLoggerName
 
 # If testfixtures in not available, skip these tests
 SKIP_TEST = False
 try:
-    import testfixtures
+    from testfixtures import LogCapture
 except ImportError:
     SKIP_TEST = True
 
@@ -12,6 +13,10 @@ except ImportError:
 @unittest.skipIf(SKIP_TEST, 'Testfixtures module not found')
 class LoggerTests(unittest.TestCase):
     '''Test if MgProxy correctly logs messages to console'''
+
+    def setUp(self):
+        self.log_capt = LogCapture(MG_LOGGER_CONST['base_name'])
+
 
     def test_delete(self):
         self.assertTrue(False)
