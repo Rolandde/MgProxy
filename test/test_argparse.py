3b24342bb9d6d7057ef5b04ce429e2a01dc83b1b
Savo Lazic <lazicsavo@gmail.com>
2015-01-03 15:35:30 -0500

    Added test for providing alternative file path. Currently fails.

--- test/test_argparse.py
+++ test/test_argparse.py
@@ -1,5 +1,6 @@
 '''Tests that args provided by user are correctly parsed and processed'''
 import unittest
+import sys
 
 from src.argv_input import arg_parser
 from src.mg_proxy_creator import createMgInstance, getFileNamePath
@@ -32,6 +33,30 @@ class TestArgs(unittest.TestCase):
         )
 
         self.assertEqual(results[1], 'yay_santa')
+
+    def test_opt_file_path(self):
+        '''Test that the user can provide optional file path'''
+        abs_path = vars(arg_parser.parse_args(
+            ['name_program', '--'+CON.ARG_CONST['opt_path'], '/opt/bin/t.txt']
+        ))
+
+        results = getFileNamePath(
+            'does/not/matter',
+            abs_path
+        )
+
+        self.assertEqual(results[0], '/opt/bin/')
+
+        rel_path = vars(arg_parser.parse_args(
+            ['name_program', '--'+CON.ARG_CONST['opt_path'], 'opt/bin/t.txt']
+        ))
+
+        results = getFileNamePath(
+            'does/not/matter',
+            rel_path
+        )
+
+        self.assertEqual(results[0], sys.argv[0] + '/opt/bin/')
 
 
 if __name__ == '__main__':
